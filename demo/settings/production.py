3501d1d773b66af8aed71c0de0d012a607182a36
Denys <denys@MacBook-Pro-Denys.local>
2024-01-12 17:11:30 +0200

    Create basic wagtail application and configure traefik as a reverse proxy

---
+++ demo/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY", "$1hg342ms&mcoi=%2(a3t=xw&5#&qvl&gmg94e(b+q$8!dux62"
+)
+
+ALLOWED_HOSTS = ["0.0.0.0"]
+
+try:
+    from .local import *
+except ImportError:
+    pass
